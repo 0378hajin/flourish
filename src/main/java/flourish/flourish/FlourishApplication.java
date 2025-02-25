a69ad55f22708974913e3322fbd98a7b6a0cbbdc
김하진 <hs@gimhajin-ui-MacBookPro.local>
2025-02-25 22:41:33 +0900

    Initial commit

---
+++ src/main/java/flourish/flourish/FlourishApplication.java
@@ -0,0 +1,13 @@
+package flourish.flourish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlourishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlourishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flourish/flourish/FlourishApplicationTests.java
@@ -0,0 +1,13 @@
+package flourish.flourish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlourishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
